c747aebb3fc7046c5572e64675d5e46d8e064928
minosRawlay <phael1681512@gmail.com>
2025-11-21 16:21:20 +0100

    init4

--- src/main/java/fr/askig2i/ascig2i/model/User.java
+++ src/main/java/fr/askig2i/ascig2i/model/User.java
@@ -1,8 +1,12 @@
 package fr.askig2i.ascig2i.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
-
+    @Id
+    private Long id;
+    @Column(name = "")
 }
