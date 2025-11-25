90d4572f839d9d1e8efd770ebd237bfcce408f82
SLCXelasflame <xelasflame05@gmail.com>
2025-11-25 15:27:30 +0100

    clean

--- src/main/java/fr/askig2i/ascig2i/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package fr.askig2i.ascig2i;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
